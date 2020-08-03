066d158193af77aae68d15875f30e24dd6d18604
darkonion <pawkapl89@gmail.com>
2020-08-03 13:56:59 +0200

    Reactive card - working but very unstable

--- src/main/java/com/example/quarkus/app/CardController.java
+++ src/main/java/com/example/quarkus/app/CardController.java
@@ -32,37 +32,37 @@ public class CardController {
 
     @Route(path = "/card/persist", methods = {HttpMethod.GET})
     public void redisAddTest(RoutingContext rc) {
-
         sessionProducer.getSessionHandler().handle(rc);
-        Session session = ofNullable(rc.session())
-                .orElseThrow(() -> new IllegalStateException());
 
+        Session session = ofNullable(rc.session())
+                .orElseThrow(() -> new IllegalStateException("Session not found. This request cannot be processed"));
+
+        //bootstrap for tests
         Product product1 = Product.builder().amount(5).itemId(1L).build();
         Product product2 = Product.builder().amount(3).itemId(2L).build();
 
         session.put("card", List.of(product1, product2));
 
         sessionProducer.getSessionStore().put(session, AsyncResult::succeeded);
-        rc.response()
-                .putHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-                .end("{ \"response\" : \"OK\" }");
+
+        rc.response().end("OK");
     }
 
     @Route(path = "/card/get", methods = {HttpMethod.GET})
     public void redisGetTest(RoutingContext rc) {
+        sessionProducer.getSessionHandler().handle(rc);
 
-        List<Product> productList = null;
-        if (nonNull(rc.session())) {
-            productList = (List<Product>) ofNullable(rc.session().get("card")).orElse(emptyList());
+        List<Product> productList;
+        if (nonNull(rc.session()) && nonNull(rc.session().get("card"))) {
+            productList = rc.session().get("card");
         } else {
             productList = emptyList();
         }
+        cardDtoService.getCard(productList, rc).subscribe().with(card -> {
 
-        cardDtoService.getCard(emptyList(), rc).subscribe().with(card -> {
             rc.response()
                     .putHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-                    .end(Json.encodePrettily(card), AsyncResult::succeeded);
-        }, fail -> System.out.println(fail));
+                    .end(Json.encodePrettily(card));
+        }, fail -> new IllegalStateException(fail.getMessage()));
     }
-
 }
